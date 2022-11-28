0c7dec806956746dccfda7290b69cf19c035d689
serpentsupercell <serpentsupercell@gmail.com>
2022-11-29 00:21:00 +0530

    3

--- 28_11_22/codes.c
+++ 28_11_22/codes.c
@@ -15,12 +15,39 @@ while(a!=0){
 printf("\nSum of digits of %d is : %d",b,sum);
 
 
+//Q. 16
+
+int num, rev = 0, rem;
+printf("\nEnter number : ");
+scanf("%d",&num);
+
+while(num != 0){
+    rem = num % 10;
+    rev = rev * 10 + rem; 
+    num = num / 10;
+}
+
+printf("\nReverse is : %d",rev);
 
 
+//Q. 18
 
+int num2,freqif = 0, freq, rem2;
+printf("\nEnter number : ");
+scanf("%d",&num2);
 
+printf("\nEnter digit to find frequency of : ");
+scanf("%d",&freq);
 
+while (num2!=0){
+    rem2 = num2 % 10;
+    num2 = num2 / 10;
+    if(freq == rem2){
+        freqif++;
+    }
+}
 
+printf("\nFrequency of %d is : %d", freq, freqif);
 
 return 0;
 }
